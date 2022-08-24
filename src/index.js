9b046dcf1c00fd399eaf12884379a5a6b5ba0b8d
suyash-patil <suyashpatil532001@gmail.com>
2022-08-25 00:28:30 +0530

    configuring theme according to baseui

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { BaseProvider, LightTheme } from 'baseui';
+import { SnackbarProvider } from "baseui/snackbar";
+
+const engine = new Styletron();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+          <BaseProvider theme={LightTheme}>
+              <SnackbarProvider>
+                      <App />
+              </SnackbarProvider>
+          </BaseProvider>
+      </StyletronProvider>
   </React.StrictMode>
 );
 
